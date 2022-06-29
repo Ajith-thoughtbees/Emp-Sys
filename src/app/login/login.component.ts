1a3d18dc35c07ffb36854e39299bc35baaf4df53
unknown <ajith.m@thoughtbees.com>
2022-06-29 07:19:42 +0530

    Test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,7 +17,8 @@ export class LoginComponent implements OnInit {
   role : string = '';
   user : User = new User();
   roles : string[];
-  
+  loginForm!: FormGroup;
+  submitted = false;
 
   constructor(private LoginService : LoginService, private route : Router, private formBuilder:FormBuilder ) {
     this.roles = [
@@ -27,39 +28,62 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.username = '';
-    this.password = '';
+    
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      role:['',Validators.required]
+  });
+}
+get f() { return this.loginForm.controls; }
+login() {
+
+  this.submitted = true;
+
+  // stop here if form is invalid
+  if (this.loginForm.invalid) {
+      return;
   }
+  if (this.loginForm.valid) {
+    if (this.loginForm.value.password === this.loginForm.value.password){
 
-  login() {
-    this.user.username = this.username;
-    this.user.password = this.password;
-    this.user.role = this.role;
+  this.LoginService.login(this.loginForm.value).subscribe(res => {
 
-    this.LoginService.login(this.user).subscribe(res => {
-
-      if(res == null) {
-        alert("Uername or password is wrong");
-        this.ngOnInit();
-      }else {
-        console.log("Login successful");
-        localStorage.setItem("empId",res.id);
-
-        if(this.role == 'Employee') {
-          this.route.navigate(['employee-dashboard']);
-        }
-
-        if( this.role == 'Admin') {
-          this.route.navigate(['dashboard']);
-        }
-
+    if(res == null) {
+      alert("Uername or password is wrong");
+      this.ngOnInit();
+    }else {
+      console.log("Login successful");
+      localStorage.setItem("empId",res.id);
+    
+      if(this.role == 'Employee') {
+        this.route.navigate(['employee-dashboard']);
+        (this.f['username'].value, this.f['password'].value == "empID")
       }
 
-    }, err => {
-      alert("Login failed");
-      this.ngOnInit();
-    })
+      if( this.role == 'Admin') {
+        this.route.navigate(['dashboard']);
+        (this.f['username'].value, this.f['password'].value == "Admin")
+      }
+    
+    }
 
-  }
-
+  }, err => {
+    alert("Login failed");
+    this.ngOnInit();
+  })
 }
+  }
+}
+}
+
+//  if (this.RegistrationForm.valid) {
+//   if (this.RegistrationForm..value.password1 === this.RegistrationForm.value.password2) {
+//     this.MyService.Register(this.RegistrationForm.value)
+//       .subscribe(
+//         (data) => {}, 
+//         error => this.errorMessage = error
+//       )
+//   } else {
+//     this.errorMessage = "cdscs";
+
